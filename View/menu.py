6369823f1ebc739d0f79f9d1c1c76291e27a7e25
di0nar4p <glaucoplay@live.com>
2023-04-01 17:02:07 -0300

    Menu

---
+++ View/menu.py
@@ -0,0 +1,100 @@
+from Controller.requisicoes import select_all,select_cpf,insert_prof,insert_aluno
+from Models.aluno import Aluno
+from Models.professor import Professor
+import os
+
+# Menu principal
+def menu():
+    while True:
+        op= int(input('''Selecione uma operação: 
+                        1- Consultar. 
+                        2- Inserir.
+                        0- Sair.
+                         
+                         '''))
+        match op:
+            case 1:
+                os.system('cls')or None
+                consulta= int(input('''                      
+                1- Consultar alunos
+                2- Consultar professores                
+                0- Sair       
+                      '''))
+                
+                
+                
+                if consulta==1:
+                    os.system('cls')or None
+                    busca= int(input('''
+                    1- Consultar todos
+                    2- Consultar por CPF
+                    0- Sair     
+                    '''))
+                    
+                    if busca == 1:
+                        os.system('cls')or None
+                        select_all('alunos')
+                    elif busca == 2:
+                        os.system('cls')or None
+                        cpf= str(input('Digite o CPF do aluno: '))
+                        select_cpf('alunos',cpf)
+                    elif busca == 0:
+                        break
+                    
+                if consulta==2:
+                    os.system('cls')or None
+                    busca= int(input('''
+                    1- Consultar todos
+                    2- Consultar por CPF
+                    0- Sair     
+                    '''))
+                    
+                    if busca == 1:
+                        os.system('cls')or None
+                        select_all('professores')
+                    if busca == 2:
+                        os.system('cls')or None
+                        cpf= str(input('Digite o CPF do professor: '))
+                        select_cpf('professores',cpf)
+                         
+                
+                elif consulta==0:
+                    break
+           
+            case 2:
+                os.system('cls')or None
+                insercao= int(input('''                      
+                1- Inserir aluno.
+                2- Inserir professor.                
+                0- Sair.       
+                      '''))
+                
+                if insercao == 1:
+                    os.system('cls')or None
+                    nome=str(input('Nome: '))
+                    idade=int(input('Idade: '))
+                    cpf=str(input('CPF: '))
+                    tel= str(input('Telefone: '))
+                    sexo=str(input('Sexo: '))
+                    matricula=str(input('Matricula: '))
+                    turma=str(input('Turma: '))
+                    aluno = Aluno(nome,idade,cpf,tel,sexo,matricula,turma)
+                    insert_aluno(aluno)
+                
+                
+                if insercao == 2:
+                    os.system('cls')or None
+                    nome=str(input('Nome: '))
+                    idade=int(input('Idade: '))
+                    cpf=str(input('CPF: '))
+                    tel= str(input('Telefone: '))
+                    sexo=str(input('Sexo: '))
+                    formacao=str(input('Formação: '))
+                    valHora=float(input('Valor hora: '))
+                    prof=Professor(nome,idade,cpf,tel,sexo,formacao,valHora)
+                    insert_prof(prof)
+            
+            case 0:
+                break  
+            
+            
